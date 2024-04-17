a665c296c371e3608d6e262872a0e6106093801e
Tobias Moyano <tobiasmoyano09@gmail.com>
2024-04-17 16:54:54 -0300

    entregable clase 10

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 import express from 'express';
+import { create } from 'express-handlebars';
+import http from 'http';
+import socketio from 'socket.io';
 import ProductManager from './ProductManager.js';
 
 const productManager = new ProductManager();
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const hbs = create({
+  extname: '.handlebars',
+  defaultLayout: 'main' // O el nombre del layout principal que quieras utilizar
+});
+
+app.engine('.handlebars', hbs.engine);
+app.set('view engine', '.handlebars');
+app.set('views', './views');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -26,7 +40,15 @@ app.get('/products/:pid', async (req, res) => {
   res.send({ status: 'success', payload: productFound });
 });
 
-app.listen(8080, (err) => {
+io.on('connection', (socket) => {
+  console.log('Un cliente se ha conectado');
+
+  socket.on('new product', (productData) => {
+    io.emit('update products', productData);
+  });
+});
+
+server.listen(8080, (err) => {
   if (err) console.log(err);
   console.log('Servidor escuchando en el puerto 8080');
 });
